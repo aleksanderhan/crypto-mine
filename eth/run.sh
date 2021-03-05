fc02b7f660af5da13d636476a4bbf614f498a61f
Aleksander Hansen <aleksanderhan@gmail.com>
2021-03-05 23:47:58 +0100

    new wallet

--- eth/run.sh
+++ eth/run.sh
@@ -1,1 +1,1 @@
-./ethminer/bin/ethminer --cuda -P stratum1+tcp://0x5D8ab96C816331416ede77635C4b58e991453e78@eu1.ethermine.org:4444
+./ethminer/bin/ethminer --cuda -P stratum1+tcp://0x1f18440CC37431ed7C7B1E95CCf456889c26EE54@eu1.ethermine.org:4444
