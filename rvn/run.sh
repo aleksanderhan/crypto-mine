04db00acaa5bfad93de7be63ea18696735b9175e
Aleksander Hansen <aleksanderhan@gmail.com>
2021-03-09 21:02:12 +0100

    wip

--- rvn/run.sh
+++ rvn/run.sh
@@ -1,1 +1,1 @@
-./NBMiner_Linux/nbminer -a kawpow -o stratum-ravencoin.flypool.org:3333 -u RQaiz1xDcfRjhoqRhfc6kRBsLYQsBrCDqS.r9: --platform 2
+./NBMiner_Linux/nbminer -a kawpow -o stratum-ravencoin.flypool.org:3333 -u RQaiz1xDcfRjhoqRhfc6kRBsLYQsBrCDqS.r9: --platform $GPU_PLATFORM
