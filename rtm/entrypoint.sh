49b0cea7c391362101148a01ccad13fe29f1759b
Aleksander Hansen <aleksanderhan@gmail.com>
2021-11-28 02:05:52 +0100

    worker name

--- rtm/entrypoint.sh
+++ rtm/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo "{
+  \"url\": \"stratum+tcps://eu.flockpool.com:5555\",
+  \"url-backup\": \"stratum+tcps://us.flockpool.com:5555\",
+  \"user\": \"RFmnEH4qfyX8qUsFFsSuKwkJPSh1Xg7NeZ.$1\",
+  \"pass\": \"x\",
+  \"algo\": \"gr\"
+}" > config.json
+
 ./cpuminer-gr-1.2.4.1-x86_64_linux/cpuminer.sh
