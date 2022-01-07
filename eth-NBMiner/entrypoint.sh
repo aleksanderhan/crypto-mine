a2578f0981965e7ef34532e53e09fb0619bb0bc5
Aleksander Hansen <aleksanderhan@gmail.com>
2022-01-07 20:38:11 +0100

    lhr mode 0

--- eth-NBMiner/entrypoint.sh
+++ eth-NBMiner/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./NBMiner_Linux/nbminer -a ethash -o ethproxy+tcp://eu1.ethermine.org:4444 -u 0x38Ce0D835ec4feEd3698516a40F155B567f8ebB2.$1 --platform $GPU_PLATFORM
+./NBMiner_Linux/nbminer -a ethash -o ethproxy+tcp://eu1.ethermine.org:4444 -u 0x38Ce0D835ec4feEd3698516a40F155B567f8ebB2.$1 --platform $GPU_PLATFORM --lhr 0
