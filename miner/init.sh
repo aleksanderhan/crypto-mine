168726eb9ac9a734ebe0ee2fb43ac9aa84e6e608
Aleksander Hansen <aleksanderhan@gmail.com>
2022-01-08 12:55:44 +0100

    adjustments

--- miner/init.sh
+++ miner/init.sh
@@ -6,4 +6,4 @@ sudo apt install -y nvidia-driver-470-server \
 
 wget https://github.com/NebuTech/NBMiner/releases/download/v40.1/NBMiner_40.1_Linux.tgz
 
-tar zxvf NBMiner_40.1_Linux.tgz .
+tar zxvf NBMiner_40.1_Linux.tgz
