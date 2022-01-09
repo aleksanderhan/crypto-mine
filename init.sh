d6b23b3e2a66d5d0b456a22b0eac55f7e9f2362e
Aleksander Hansen <aleksanderhan@gmail.com>
2022-01-10 00:01:57 +0100

    -containerd.io

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list |\
     sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 sudo apt update
-sudo apt install nvidia-container-runtime containerd.io
+sudo apt install nvidia-container-runtime
 
 sudo systemctl stop docker
 sudo systemctl start docker
