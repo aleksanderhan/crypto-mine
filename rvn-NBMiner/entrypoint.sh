234dbdcecc13c5ff29ce53a409fde81c521057ea
Aleksander Hansen <aleksanderhan@gmail.com>
2022-01-10 01:07:49 +0100

    wip

--- rvn-NBMiner/entrypoint.sh
+++ rvn-NBMiner/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./NBMiner_Linux/nbminer -a kawpow -o stratum-ravencoin.flypool.org:3333 -u RQaiz1xDcfRjhoqRhfc6kRBsLYQsBrCDqS.$1: --platform $GPU_PLATFORM
+./NBMiner_Linux/nbminer -a kawpow -o stratum-ravencoin.flypool.org:3333 -u RQaiz1xDcfRjhoqRhfc6kRBsLYQsBrCDqS.$1 --platform $GPU_PLATFORM
