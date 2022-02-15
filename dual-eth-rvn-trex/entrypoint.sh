0690401a1b951e33a9d00f94f5ac42faa0f8187c
Aleksander Hansen <aleksanderhan@gmail.com>
2022-02-15 22:22:49 +0100

    dual mining eth+rvn

---
+++ dual-eth-rvn-trex/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./t-rex -a ethash --lhr-algo kawpow -o ethproxy+tcp://eu1.ethermine.org:4444 -u 0x38Ce0D835ec4feEd3698516a40F155B567f8ebB2 -p x -w $1 --url2 stratum-ravencoin.flypool.org:3333 --user2 RQaiz1xDcfRjhoqRhfc6kRBsLYQsBrCDqS.$1 --pass2 x
