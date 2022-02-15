2874bd01e3ed90ed342b35797b46d37ba694c706
Aleksander Hansen <aleksanderhan@gmail.com>
2022-02-15 22:33:54 +0100

    cmd

--- dual-eth-rvn-trex/entrypoint.sh
+++ dual-eth-rvn-trex/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./t-rex -a ethash --lhr-algo kawpow -o ethproxy+tcp://eu1.ethermine.org:4444 -u 0x38Ce0D835ec4feEd3698516a40F155B567f8ebB2 -p x -w $1 --url2 stratum-ravencoin.flypool.org:3333 --user2 RQaiz1xDcfRjhoqRhfc6kRBsLYQsBrCDqS.$1 --pass2 x
+./t-rex -a ethash --lhr-algo kawpow -o ethproxy+tcp://eu1.ethermine.org:4444 --user 0x38Ce0D835ec4feEd3698516a40F155B567f8ebB2.$1 --pass x --url2 stratum-ravencoin.flypool.org:3333 --user2 RQaiz1xDcfRjhoqRhfc6kRBsLYQsBrCDqS.$1 --pass2 x
